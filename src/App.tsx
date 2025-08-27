01215686c763474ac34d20d97d707fd6cec1acfb
김무진 <anwls5928@naver.com>
2025-08-27 16:33:16 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const StBox = styled.div`
+  /
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+  margin: 20px;
+  display: flex;
+`;
+
+const App = () => {
+  return <StBox>박스</StBox>;
+};
 
 export default App;
